245b21a872e9201981985e6e2e84d8747c7703cf
Christopher Sesugh <christophersesugh@Christophers-MacBook-Air.local>
2023-01-20 16:01:29 +0100

    add profile page

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,48 +3,51 @@ import AppHead from "components/app-head";
 import PageHeader from "components/page-header";
 import Image from "next/image";
 import { Form } from "components/login";
+import { Fade } from "react-awesome-reveal";
 
 export default function Login() {
   const [registered, setRegistered] = React.useState(true);
   return (
     <>
       <AppHead title="Login | CAS" />
-      <div className="px-4 w-full">
-        <PageHeader
-          headerImage={
-            <Image
-              src="/assets/login.jpeg"
-              width={300}
-              height={200}
-              alt="login"
-              className="self-start rounded-xl"
-            />
-          }
-          title="Log in to your account."
-          subtitle="Or signup for an account."
-        >
-          <div className="w-[80%] mt-12">
-            <Form registered={registered} />
-            <div className="mt-8 text-sm">
-              <p>
-                {registered
-                  ? "Don't have an account ?"
-                  : "Already have an account ?"}{" "}
-                <button
-                  className="underline"
-                  onClick={() => setRegistered(!registered)}
-                >
-                  {registered ? "Register" : "Login"}
-                </button>
-              </p>
+      <Fade cascade duration={1000}>
+        <div className="px-4 w-full">
+          <PageHeader
+            headerImage={
+              <Image
+                src="/assets/login.jpeg"
+                width={300}
+                height={200}
+                alt="login"
+                className="self-start rounded-xl"
+              />
+            }
+            title="Log in to your account."
+            subtitle="Or signup for an account."
+          >
+            <div className=" w-full md:w-[80%] mt-12">
+              <Form registered={registered} />
+              <div className="mt-8 text-sm">
+                <p>
+                  {registered
+                    ? "Don't have an account ?"
+                    : "Already have an account ?"}{" "}
+                  <button
+                    className="underline"
+                    onClick={() => setRegistered(!registered)}
+                  >
+                    {registered ? "Register" : "Login"}
+                  </button>
+                </p>
+              </div>
             </div>
-          </div>
-          <p className="self-center text-lg text-slate-400 mt-20">
-            To sign in to your account or to create a new one fill in your email
-            and password above and {"we'll"} get you started.
-          </p>
-        </PageHeader>
-      </div>
+            <p className="self-center text-lg text-slate-400 mt-20">
+              To sign in to your account or to create a new one fill in your
+              email and password above and {"we'll"} get you started.
+            </p>
+          </PageHeader>
+        </div>
+      </Fade>
     </>
   );
 }
--- pages/me.tsx
+++ pages/me.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Me() {
   const router = useRouter();
-  let user = false;
+  let user = true;
   React.useLayoutEffect(() => {
     if (!user || user === null) {
       router.push("/login");
@@ -28,7 +28,43 @@ export default function Me() {
         }
         title="Your profile."
         subtitle="You can edit your profile or connect to our discord server to intereact with like minded people."
-      ></PageHeader>
+      >
+        <div className="mt-12 w-full md:w-[50%]">
+          <ul>
+            <li className="rounded-lg bg-zinc-300">
+              <span className="bg-zinc-500 h-full inline-block p-2 rounded-lg text-slate-300">
+                Email:
+              </span>
+              <span className="p-2 text-zinc-600">
+                christohybrid185@gmail.com
+              </span>
+            </li>
+            <li className="text-lg text-zinc-400 mt-8">
+              <form>
+                <label htmlFor="password" className="text-sm">
+                  Change your password
+                </label>
+                <input
+                  type="text"
+                  required
+                  placeholder="Password"
+                  id="password"
+                  className="w-full p-2 text-slate-200 rounded-lg outline-none bg-slate-400 placeholder-slate-200"
+                />
+                <button
+                  type="submit"
+                  className="text-blue-400 border-2 p-2 rounded-lg mt-4"
+                >
+                  Save
+                </button>
+              </form>
+            </li>
+            <li className="text-lg text-zinc-400 mt-8">
+              <button className="underline">Connect to Discord</button>
+            </li>
+          </ul>
+        </div>
+      </PageHeader>
     </>
   );
 }
